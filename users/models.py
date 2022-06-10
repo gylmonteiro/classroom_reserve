3b966f004c959b26fd0a619fd7695a8562d28d9f
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2022-06-10 20:05:21 -0300

    fix: alter registration column to unique

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Users(models.Model):
-    registration = models.IntegerField(blank=False, null=False)
+    registration = models.IntegerField(blank=False, null=False, unique=True)
     first_name = models.CharField(blank=False, null=False, max_length=255)
     last_name = models.CharField(blank=False, null=False, max_length=255)
 
